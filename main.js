d6be4966372dd6be7e9a95f4c7ab11f7fe3c24ca
empolem33 <inspiron0925@yahoo.com>
2022-12-27 16:53:57 -0800

    save state added

---
+++ main.js
@@ -0,0 +1,47 @@
+const simpleLevelPlan = `
+......................
+..#................#..
+..#..............=.#..
+..#.........o.o....#..
+..#.@......#####...#..
+..#####............#..
+......#++++++++++++#..
+......##############..
+......................`;
+
+
+let Level = class Level {
+    constructor(plan) {
+        let rows = plan.trim().split('\n').map(l => [...l]);
+        this.height = rows.length;
+        this.width = rows[0].length;
+        this.startActors = [];
+
+        this.rows = rows.map((row, y) =>{
+            return row.map((ch, x) =>{
+                let type = levelChars[ch];
+                if(typeof type == 'string') return type;
+                this.startActors.push(
+                    type.create(new Vec(x,y), ch)
+                )
+                return "empty";
+            })
+        })
+    }
+}
+
+let State = class State {
+    constructor(level, actors, status){
+        this.level = level
+        this.actors = actors
+        this.status = status
+    }
+
+    static start(level) {
+        return new State(level, level.startActors, "playing");
+    }
+
+    get Player(){
+        return this.actors.find(a=> a.type === "player")
+    }
+}
